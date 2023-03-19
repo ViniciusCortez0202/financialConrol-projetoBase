12844a242ee2d215efba4be6ee3299910610a40f
= <cortezvinicius11@gmail.com>
2023-03-19 12:32:43 -0300

    create spentCard

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SpentCard from './src/components/SpentCard';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SpentCard spentName={"Aluguel"} spentValue={300}/>
     </View>
   );
 }
---
+++ src/components/SpentCard/style.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    spentCard: {
+        width: '100%',
+        borderRadius: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 15,
+        backgroundColor: '#e3e3e3',
+        elevation: 4
+    },
+    spentName: {
+        fontSize: 20,
+        fontWeight: '700',
+        color: '#000',
+        opacity: 0.75
+    },
+    spentValue: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#000',
+        opacity: 0.95
+    }
+});
